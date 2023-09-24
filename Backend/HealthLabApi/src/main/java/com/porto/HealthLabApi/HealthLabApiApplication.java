400007f154973fe1626b6adbe55883720e7d916f
Vinícius Porto <81120004+ViniPorto@users.noreply.github.com>
2023-09-24 19:42:01 -0300

    Add files via upload

---
+++ Backend/HealthLabApi/src/main/java/com/porto/HealthLabApi/HealthLabApiApplication.java
@@ -0,0 +1,13 @@
+package com.porto.HealthLabApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthLabApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthLabApiApplication.class, args);
+	}
+
+}
---
+++ Backend/HealthLabApi/src/test/java/com/porto/HealthLabApi/HealthLabApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porto.HealthLabApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthLabApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
